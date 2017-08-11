4d8283dfb17c372a4bbbf320ad28df10877a5d38
yy1533 <yun.yan@nyumc.org>
2017-08-11 17:06:36 -0400

    :feet: see issue #12

---
+++ celseq2/cook_config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+
+def read_config_template(mode='multiple'):
+    '''
+    Locate config.yaml inside pkg folder
+    '''
+    pass
+
+
+
+def create_config_file(mode='multiple'):
+    '''
+    Locate config.yaml template and export to end-users
+    '''
+    pass
+
+
+
+def main():
+    '''
+    main function for cook config
+    '''
+    pass
